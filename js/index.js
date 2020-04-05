092871bc8dd76fb94dd87a567da42a90c1d9de54
jsjxhgw_global <2937953304@qq.com>
2020-04-05 23:34:40 +0800

    计算机协会项目第8次提交--2020/4/5 23:34 --czy

--- js/index.js
+++ js/index.js
@@ -50,21 +50,54 @@ function foLogin() {
 /*********吸顶效果**********/
 $(function(){
 
-    var ceilingTop = $('.ceiling').offset().top;
+    var ceilingTop = $('.floor').offset().top;
 
     $(document).on('scroll',function(){
         if($(document).scrollTop() > ceilingTop){
-            $('.ceiling').css({
+            $('.floor').css({
                 "position":"fixed",
                 "top":0,
             })
         }else{
 
-            $('.ceiling').css({
+            $('.floor').css({
                 "position":"absolute",
                 "top":"557px",
                 "background-color":"#8c8c8c",
             })
         }
     })
+})
+
+/*********楼层特效**********/
+//          第一种点击li元素到相应的楼层的方法,可以直接用锚点的方式,然后加上点击后的效果
+//          $(".floor li").click(function(){
+//              $(this).addClass("active").siblings().removeClass("active")
+//          })
+
+//          第二种点击li元素到相应的楼层的方法，锚点要去掉
+var flag = true //设置标识。防止出现跑马灯
+$(".floor li").click(function(){
+    flag = false
+    $(this).addClass("active").siblings().removeClass("active")
+    var index = $(this).index()//获取当前点击元素的索引
+    var top = $(".banner").eq(index).offset().top;//获取每个banner到顶部的距离
+    $("html,body").stop(true).animate({"scrollTop":top},function(){flag = true})
+})
+
+//          滚轮滑动切换楼层
+$(window).scroll(function(){
+    if(flag){
+        //浏览器可视窗口的一半，也可以根据自己需求设定
+        var winH = $(window).innerHeight()/1;
+        var scrollT = $(window).scrollTop()
+        var len = $(".banner").size()
+        for(var i=0;i<len;i++){
+            //注意这里banner对象加了i之后变成了js对象,所以用offsetTop
+            var bannerGap = $(".banner")[i].offsetTop - scrollT
+            if(bannerGap < winH){
+                $(".floor li").eq(i).addClass("active").siblings().removeClass("active")
+            }
+        }
+    }
 })
